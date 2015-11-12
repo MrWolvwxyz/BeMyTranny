bcaeb93ead89deb8a229880d41016dbd7fdc1c49
Riya-Gao <wenjungaoat@gmail.com>
2015-11-11 22:38:08 -0500

    css_is_not_working

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Sam/Desktop/BeMyTranny/flask"
+set -gx VIRTUAL_ENV "/Users/riya/BeMyTranny/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
