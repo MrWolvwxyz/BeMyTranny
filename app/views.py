daa0515556b1667e48dcae77957a7f56b8889bfb
Mo <themohussein@gmail.com>
2015-11-17 02:17:12 -0500

    added login page and basic attempted functionality

--- app/views.py
+++ app/views.py
@@ -1,8 +1,29 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, redirect, url_for, request, session, flash
 
 @app.route('/')
 @app.route('/index.html')
 def index():
     return render_template('index.html',
                            title='Home')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin':
+            error = 'Invalid credentials'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    error = None
+    #session.pop('logged_in', None) #pops 'True' value off and replace with None
+    return render_template('logout.html', error=error)
+
+def return_home():  
+    return redirect(url_for('home')) #find a better redirect?
+
