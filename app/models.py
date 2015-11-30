53f16826d44dd85c17b9cdd2e45181eff9e2dce1
Mo <themohussein@gmail.com>
2015-11-29 19:20:43 -0500

    added schema for translations&posts

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=False)
     pw_hash = db.Column(db.String(70), index=False, unique=False)
 #len(sha256.hexdigest()) = 64
-    #posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     @property
     def is_authenticated(self):
@@ -29,4 +29,14 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.username)
 
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=True, unique=False)
+    description = db.Column(db.String(1024), index=True, unique=False)
+    name = db.Column(db.String(120), index=True, unique=False) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #id of uploader
 
+class Translations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id')) #id of uploader
+    translation = db.Column(db.String(1024), index=False, unique=False)
