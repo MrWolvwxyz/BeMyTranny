1fb35f18baa02a4ffc6faf14db616f306ac75eae
Riya-Gao <wenjungaoat@gmail.com>
2015-12-06 14:05:58 -0500

    add redirect page

--- web/controllers/ask.py
+++ web/controllers/ask.py
@@ -32,7 +32,7 @@ def askfunc():
             cur.execute("insert into post (title, description, origin, target, pathtophoto, userid) values ('" + title + "', '" + des + "', '" + ori + "', '" + tar + "', '" + newpic.filename +"', "+ currentuserid +");")
             cur.execute("commit")
 
-        return render_template('ask.html',  error=error)
+        return render_template('redirect.html',  error=error)
 
     print ("route = ask")
 
