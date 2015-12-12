da9abb89e655d27330ff49c2803071d0e4f6a800
Mo <themohussein@gmail.com>
2015-12-12 17:23:20 -0500

    better password retrieval

--- web/app.py
+++ web/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import controllers
 import sys
+from getpass import getpass
 from extensions import mysql
 app = Flask(__name__, template_folder='templates')
 
@@ -17,7 +18,7 @@ app.config['MYSQL_USER']='root'
 try:
     pw = str(sys.argv[1]) #password supplied as cmd line arg
 except:
-    pw = str(raw_input("input mysql password:")) #or input later
+    pw = getpass("input password: ")
 app.config['MYSQL_PASSWORD']=pw
 app.config['MYSQL_DB']='bmt'
 app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
